9075085c5f7d54b40cf70f3bc00c699a03f28b3a
perttuj <perttu_1995@hotmail.com>
2018-06-20 17:21:12 +0200

    init
    added codeigniter fw, base page and folders

---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Välkommen till CityEl!</title>
+</head>
+<body>
